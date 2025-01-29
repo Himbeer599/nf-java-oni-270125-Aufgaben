800b8063c3a6c5b30faf2cd8abe276081fc2411a
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-01-29 12:04:15 +0100

    Add CharPrinter method

---
+++ src/main/java/arrays/CharacterPrinter.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class CharacterPrinter {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a text: ");
+
+        String inputText = scanner.nextLine();
+
+        System.out.println("The characters in the input text are: ");
+        for (int i =0; i<inputText.length(); i++){
+            System.out.println(inputText.charAt(i));
+        }
+
+        //Die zweite Formen
+        System.out.println("Zweite Formen");
+        for (char text: inputText.toCharArray()){
+            System.out.println(text);
+        }
+        scanner.close();
+    }
+}
