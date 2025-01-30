c036a9b756be817cf23c15b1490ac5e29ea1fc12
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-01-30 12:12:10 +0100

    Add Third minimal implementation (move W)

--- src/main/java/test/PlayerCharacter.java
+++ src/main/java/test/PlayerCharacter.java
@@ -1,10 +1,33 @@
 package test;
 
-public class PlayerCharacter {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class PlayerCharacter {
+    static int x = 0;
+    static int y = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter: ");
+
+        String inputText = scanner.nextLine();
+        scanner.close();
+//
     }
-    public static int getX(int number) {
-        return 0;
+
+    public static int getX() {
+        return x;
+    }
+
+    public static int getY() {
+        return y;
+    }
+
+    public static boolean move(String str) {
+        if (str == "W") {
+            y = 1;
+        }
+        return true;
     }
 }
--- src/test/java/test/GameBoardTest.java
+++ src/test/java/test/GameBoardTest.java
@@ -6,12 +6,50 @@ import org.junit.jupiter.api.Test;
 public class GameBoardTest {
 
     @Test
-    void expecteUp_whenPressW(){
-        int a = 0;
+    void expecteNull_whenGetX(){
+//        int a = 0;
         int expected = 0;
-        int actual = PlayerCharacter.getX(a);
+        int actual = PlayerCharacter.getX();
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void expecteNull_whenGetY(){
+//        int a = 0;
+        int expected = 0;
+        int actual = PlayerCharacter.getY();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void expecteTrue_whenMoveW(){
+//        int a = 0;
+        String str= "W";
+        boolean expected = true;
+        boolean actual = PlayerCharacter.move(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void expecte_whenMoveW(){
+//        int a = 0;
+        int expected = 0;
+        int actual = PlayerCharacter.getX();
+        Assertions.assertEquals(expected, actual);
+
+        expected = 0;
+        actual = PlayerCharacter.getY();
+        Assertions.assertEquals(expected, actual);
+
+        String str= "W";
+        boolean expectedmove = true;
+        boolean actualmove = PlayerCharacter.move(str);
+        Assertions.assertEquals(expectedmove, actualmove);
+
+        expected = 1;
+        actual = PlayerCharacter.getY();
+        Assertions.assertEquals(expected, actual);
+    }
+
 
 
 }
