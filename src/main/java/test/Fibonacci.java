acd8aba7cb502aaf9e9c908f77c77eaed17a5df7
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-01-30 15:08:08 +0100

    Add calculate method

--- src/main/java/test/Fibonacci.java
+++ src/main/java/test/Fibonacci.java
@@ -8,6 +8,22 @@ public class Fibonacci {
 
 
     public static int calculate(int i) {
-        return 0;
+        int a = 0;
+        int b = 1;
+        int result = 0;
+
+        if (i == 1) {
+            return a;
+        } else if (i == 2) {
+            return b;
+        }
+        for (int index = 2; index < i; index++) {
+            result = a +b;
+            a = b;
+            b = result;
+            System.out.println("a: "+a);
+            System.out.println("b: "+b);
+        }
+        return result;
     }
 }
--- src/test/java/test/FibonacciTest.java
+++ src/test/java/test/FibonacciTest.java
@@ -12,4 +12,40 @@ public class FibonacciTest {
         int actual = Fibonacci.calculate(i);
         Assertions.assertEquals(expected,actual);
     }
+
+    @Test
+    void test2(){
+        int i = 2;
+        int expected =1;
+        int actual = Fibonacci.calculate(i);
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    void test3(){
+        int i = 3;
+        int expected =1;
+        int actual = Fibonacci.calculate(i);
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    void test4(){
+        int i = 4;
+        int expected =2;
+        int actual = Fibonacci.calculate(i);
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    void test5(){
+        int i = 5;
+        int expected =3;
+        int actual = Fibonacci.calculate(i);
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    void test6(){
+        int i = 6;
+        int expected =5;
+        int actual = Fibonacci.calculate(i);
+        Assertions.assertEquals(expected,actual);
+    }
 }
